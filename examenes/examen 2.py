c9e0044c612d45d7dd79817a21ebbb41d412c31f
alejandrolopezrojas <alejandrolopezrojas2009@gmail.com>
2026-01-08 09:51:39 -0500

    terminar el examen 2

--- examenes/examen 2.py
+++ examenes/examen 2.py
@@ -1,14 +1,16 @@
-"""
+#punto 1
 nombre, año = input("introduzca su nombre= "), int(input("introduzca su año de nacimiento= "))
 print("su nombre es", nombre, type(nombre))
 print("y su edad en el 2026 es= ", 2026-año )
 
 
+"""
 #punto 2
 105
 type str
 15
 int
+"""
 
 
 
@@ -17,8 +19,18 @@ int
 frase = input("escribe una frase= ")
 valor = print("el numero de caracteres es= ", len(frase))
 print(type(valor))
+
+"""
+#punto 4
+
+siendo sincero todavia no he aprendido sobre bool
+pero creo que puedo saber.
+si el valor es 0 puede ser false o nonetype, y si el valor es 5
+el valor sera true
 """
 
 
-#punto 4
+#punto 5
 
+edad = int(input("Edad: "))
+print("El próximo año tendrás", edad + 1)
