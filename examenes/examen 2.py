dd6a6fc3780fbdad9e5a90ffad003afa9d20237c
alejandrolopezrojas <alejandrolopezrojas2009@gmail.com>
2026-01-08 09:44:22 -0500

    Agregar contenido inicial del examen 2

---
+++ examenes/examen 2.py
@@ -0,0 +1,24 @@
+"""
+nombre, año = input("introduzca su nombre= "), int(input("introduzca su año de nacimiento= "))
+print("su nombre es", nombre, type(nombre))
+print("y su edad en el 2026 es= ", 2026-año )
+
+
+#punto 2
+105
+type str
+15
+int
+
+
+
+#punto 3
+
+frase = input("escribe una frase= ")
+valor = print("el numero de caracteres es= ", len(frase))
+print(type(valor))
+"""
+
+
+#punto 4
+
