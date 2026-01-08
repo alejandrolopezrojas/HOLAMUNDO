6c61df4cab467aec93d6b35dd11b162c12f21ed6
alejandrolopezrojas <alejandrolopezrojas2009@gmail.com>
2026-01-07 20:45:57 -0500

    Agregar archivos de ejercicios de programación: punto 1 y punto 2

--- primer examen/primerexamen.py
+++
@@ -1,5 +0,0 @@
-edad= 16
-nombre= "Alejandro"
-altura= 1.83
-print("hola, mi nombre es", nombre, "mi edad es", edad, "y mi altura es", altura)
-
---
+++ primer examen/punto 1.py
@@ -0,0 +1,6 @@
+#Crea variables con: tu edad tu altura tu nombre Luego imprime una frase así: Hola, me llamo ___, tengo ___ años y mido ___ metros
+
+edad= 16
+nombre= "Alejandro"
+altura= 1.83
+print("hola, mi nombre es", nombre, "mi edad es", edad, "y mi altura es", altura)
